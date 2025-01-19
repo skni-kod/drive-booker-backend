dae7b33d8aff840d96482489f11c8cdc4936c197
Finnick223 <filipf192@gmail.com>
2025-01-19 18:05:47 +0100

    feat: create Resource for EventController index

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\EventResource;
 use App\Models\Event;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Http\Request;
@@ -14,7 +15,8 @@ class EventController extends Controller
      */
     public function index()
     {
-        return response()->json(auth()->user()->events);
+        $events = Event::all();
+        return EventResource::collection($events);
     }
 
     /**
